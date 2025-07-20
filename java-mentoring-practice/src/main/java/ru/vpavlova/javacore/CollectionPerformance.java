34040500afd8e2dd0ea97bcb6d7cbb4490cb537f
Victoria Pavlova <vikkkapaw@mail.ru>
2025-07-20 15:57:32 +0300

    Исправлена ошибка ConcurrentModificationException.

--- java-mentoring-practice/src/main/java/ru/vpavlova/javacore/CollectionPerformance.java
+++ java-mentoring-practice/src/main/java/ru/vpavlova/javacore/CollectionPerformance.java
@@ -1,45 +1,33 @@
 package main.java.ru.vpavlova.javacore;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Починить ConcurrentModificationException в многопоточной среде исполнения
  */
 public class CollectionPerformance {
     public static void main(String[] args) {
-//        addElementsPerformance();
         failFast();
     }
 
-    private static void addElementsPerformance() {
-        var arr = new ArrayList<Integer>();
-        var start = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-            arr.add(0, i);
-        }
-        System.out.println("ArrayList time taken: " + (System.currentTimeMillis() - start) + " ms");
-
-        var linkedList = new java.util.LinkedList<Integer>();
-        start = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-            linkedList.add(0, i);
-        }
-        System.out.println("LinkedList time taken: " + (System.currentTimeMillis() - start) + " ms");
-    }
-
+    // Использование CopyOnWriteArrayList
     private static void failFast() {
-        List<String> list = new ArrayList<>();
+        List<String> list = new CopyOnWriteArrayList<>();
         list.add("Apple");
         list.add("Banana");
         list.add("Cherry");
 
+        // Обычный ArrayList не потокобезопасен.
+        // Когда поток main добавлял элементы, другой - t1 пытался их удалять,
+        // поэтому и возникла ошибка ConcurrentModificationException
         Thread t1 = new Thread(() -> {
-            for(String s : list) {
-                if(s.equals("Cherry")) {
+            for (String s : list) {
+                if (s.equals("Cherry")) {
                     list.remove(s);
                 }
             }
+            System.out.println("После удаления: " + list);
         });
         t1.start();
     }
