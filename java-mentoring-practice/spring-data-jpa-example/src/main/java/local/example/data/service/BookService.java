a95ea0054074f4252ea857e7cb6c089191005d4b
Victoria Pavlova <vikkkapaw@mail.ru>
2025-08-08 14:43:27 +0300

    Добавлены комментарии с альтернативными способами решения для проблемы N+1 и LazyInitializationException.

--- java-mentoring-practice/spring-data-jpa-example/src/main/java/local/example/data/service/BookService.java
+++ java-mentoring-practice/spring-data-jpa-example/src/main/java/local/example/data/service/BookService.java
@@ -16,7 +16,20 @@ public class BookService {
 
     @Transactional(readOnly = true)
     public List<Author> getAllAuthorsWithBooks() {
-        // Решение проблемы N+1 через использование метода findAllWithBooks() с JOIN FETCH
+        /*
+         * Текущее решение проблемы N+1: JOIN FETCH через кастомный метод findAllWithBooks() в репозитории
+         *
+         * Альтернативный способ №1:
+         * В AuthorRepository добавить аннотацию @EntityGraph(attributePaths = "books") - загружаются книги вместе с авторами
+         * List<Author> findAll() и тогда в методе getAllAuthorsWithBooks() можно использовать обычный findAll()
+         *
+         * Альтернативный способ №2:
+         * Использовать @BatchSize;
+         * Добавить в сущность Author аннотацию
+         * @BatchSize(size = 10)
+         * private List<Book> books;
+         * или в application.yml hibernate.default_batch_fetch_size: 10
+         */
         List<Author> authors = authorRepository.findAllWithBooks();
 
         authors.forEach(author -> {
@@ -27,7 +40,24 @@ public class BookService {
         return authors;
     }
 
-    // Добавлена аннотация @Transactional иначе возникнет ошибка LazyInitializationException
+    /*
+     * Решения проблемы LazyInitializationException
+     *
+     * Текущее решение: Добавлена аннотация @Transactional держит сессию открытой, чтобы избежать LazyInitializationException
+     * в дальнейшем применяется метод с JOIN FETCH
+     *
+     * Можно использовать @EntityGraph - аннотация @EntityGraph(attributePaths = "books")
+     *
+     * Паттерн Self-Inject
+     * Self-Inject служит для разделения транзакций, чтобы
+     * не держать одну большую транзакцию во время долгих I/O операций;
+     * Реализация:
+     * 1. внедряем BookService через Spring Proxy
+     * @Lazy
+     * private final BookService self;
+     * 2. Вызываем метод через self
+     * authors.forEach(self::printAuthorWithBooks);
+     */
     @Transactional(readOnly = true)
     public void printAuthorsAndBooks() {
         // Используем метод с JOIN FETCH
