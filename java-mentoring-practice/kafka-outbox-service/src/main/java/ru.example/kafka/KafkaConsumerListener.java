8e9821043ca4acc16ca963e31ee29df6cd524469
Victoria Pavlova <vikkkapaw@mail.ru>
2025-08-12 12:57:38 +0300

    В классе KafkaConsumerListener реализовано ручное подтверждение оффсета (Acknowledgment) и в файле application.yml добавлены настройки по отключению авто-коммита оффсета.

--- java-mentoring-practice/kafka-outbox-service/src/main/java/ru.example/kafka/KafkaConsumerListener.java
+++ java-mentoring-practice/kafka-outbox-service/src/main/java/ru.example/kafka/KafkaConsumerListener.java
@@ -2,13 +2,19 @@ package ru.example.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumerListener {
 
     @KafkaListener(topics = "${kafka.topic}", groupId = "kafka-outbox-group")
-    public void listen(ConsumerRecord<String, String> record) {
-        System.out.println("Received message from Kafka: " + record.value());
+    public void listen(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        try {
+            System.out.println("Received message from Kafka: " + record.value());
+            ack.acknowledge(); // вручную фиксируем оффсет
+        } catch (Exception e) {
+            System.err.println("Error while processing event: " + e.getMessage());
+        }
     }
 }
