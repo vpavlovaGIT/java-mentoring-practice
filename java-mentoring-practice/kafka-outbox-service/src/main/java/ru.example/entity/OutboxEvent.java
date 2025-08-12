6f5002a251aba57cd2bdddb43147dae2a782e7c8
Victoria Pavlova <vikkkapaw@mail.ru>
2025-08-12 11:05:15 +0300

    Заменен тип данных UUID на Long для id.

--- java-mentoring-practice/kafka-outbox-service/src/main/java/ru.example/entity/OutboxEvent.java
+++ java-mentoring-practice/kafka-outbox-service/src/main/java/ru.example/entity/OutboxEvent.java
@@ -12,8 +12,8 @@ import lombok.Setter;
 public class OutboxEvent {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String payload;
 
